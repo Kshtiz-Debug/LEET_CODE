5ef33c4ec30b47e60c7dc157a5e7e7b902efd970
Prakhar <kshitiz24112000@gmail.com>
2026-04-19 22:34:05 +0530

    Add containsDuplicate function to check for duplicates
    Implement function to check for duplicates in an array.

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,16 @@
+"""
+
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+ """
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        x=set(nums)
+        if len(x)==len(nums):
+            return False
+        else:
+            return True
