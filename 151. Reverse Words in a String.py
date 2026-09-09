a6d6081e803149f111bf6714ebea5dd85705306c
Prakhar <kshitiz24112000@gmail.com>
2026-09-09 10:37:07 +0530

    Add reverseWords function to reverse string words
    Implemented a function to reverse the order of words in a string, handling leading, trailing, and multiple spaces.

---
+++ 151. Reverse Words in a String.py
@@ -0,0 +1,44 @@
+"""
+
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+
+
+"""
+
+
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        x=s.split()
+        y=len(x)
+        l=""
+        for i in range(y):
+            l=l+ " " +x[y-i-1]
+        return l.strip()
+
+
+
+
+
+
+
+"""
+
+
+from this i learnt wht is the difference between split n strip ...  
+
+
+so split is used to split the character into multiple substrings 
+while strip removes any trailing whitespace characters from the string 
+
+
+
+"""
